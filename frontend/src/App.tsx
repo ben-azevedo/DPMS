87e157b4d4f2c406e504f6f115bcaab5e259ffbb
benazevedo <ben_azevedo@outlook.com>
2024-02-16 14:05:16 -0800

    Auth Screen boilerplate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import bananaLogo from "/banana.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={bananaLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>Design Project Management System</h1>
+
+      <h3 id="user"></h3>
+
+      <form id="sign_up_form">
+        <input type="text" id="signup-email" placeholder="Email" />
+        <input type="password" id="signup-password" placeholder="Password" />
+        <input
+          type="password"
+          id="signup-password-confirm"
+          placeholder="Confirm Password"
+        />
+        <button type="submit" id="Sign Up">
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </form>
+
+      <form id="sign_in_form">
+        <input type="text" id="signin-email" placeholder="Email" />
+        <input type="password" id="signin-password" placeholder="Password" />
+        <button type="submit" id="Login">
+          Submit
+        </button>
+      </form>
+
+      <button id="sign_out">Logout</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
